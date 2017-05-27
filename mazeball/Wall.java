f198c69058e0b41a2dbdda6d6368beb4f4afa284
Axel Valverde Sanchís <axvasa25@hotmail.com>
2017-05-28 01:31:48 +0200

    Update Wall.java

--- mazeball/Wall.java
+++ mazeball/Wall.java
@@ -10,21 +10,26 @@ public class Wall extends View {
     public float inicioY, finalY;
     private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
+    //construct new ball object
     public Wall(Context context, float inicioX, float inicioY, float finalX, float finalY) {
         super(context);
-        mPaint.setColor(0xFFFFFFFF); // Color blanco para el muro
-        mPaint.setStrokeWidth(20); // Anchura del muro
-        this.inicioX = inicioX; // Posicion inicial X del muro
-        this.inicioY = inicioY; // Posicion inicial Y del muro
-        this.finalX = finalX; // Posicion inicial X del muro
-        this.finalY = finalY; // Posicion final Y del muro
+        //color hex is [transparency][red][green][blue]
+        mPaint.setColor(0xFFFFFFFF);
+        mPaint.setStrokeWidth(10);
+        this.inicioX = inicioX;
+        this.inicioY = inicioY;
+        this.finalX = finalX;
+        this.finalY = finalY;
     }
 
+    public int getStrokeWidth(){
+        return 10/2;
+    }
+
+    //called by invalidate()
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // Se dibuja una linea en las coordenadas indicadas
         canvas.drawLine(inicioX, inicioY, finalX, finalY, mPaint);
     }
-
 }
